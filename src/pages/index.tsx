a249e4dacbf10b13887329c77103093bd3a51be3
Leonardo G N Henrique <leonardognh@outlook.com.br>
2025-01-03 21:09:46 -0300

    primeiro commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,8 @@
+import type {ReactNode} from 'react';
+import { Redirect } from '@docusaurus/router';
+
+export default function Home(): ReactNode {
+  return (
+    <Redirect to="/docs/intro"/>
+  );
+}
